6debf890789d25f6224e3d5dafdab0638f18b99f
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-11-20 16:20:36 -0600

    updated manual

--- frontend/src/Containers/Manual.js
+++ frontend/src/Containers/Manual.js
@@ -8,31 +8,61 @@ import { Typography } from '@material-ui/core';
 class Manual extends Component {
     render() {
         return (
-            <div style={{marginTop: 80, textAlign: 'center'}}>
-                <Typography variant="h5">ON</Typography>
-                <Grid item xs={12} md={6} style={{margin: 'auto'}}>
-                    <ButtonGroup variant="contained" color="primary" fullWidth aria-label="full width outlined button group">
-                        <Button>Valve 1</Button>
-                        <Button>Valve 2</Button>
-                        <Button>Valve 3</Button>
-                        <Button>Valve 4</Button>
-                        <Button>Valve 5</Button>
-                        <Button>Valve 6</Button>
-                        <Button>Valve 7</Button>
-                        <Button>Valve 8</Button>
+            <div style={{marginTop: 30, textAlign: 'center'}}>
+                <Typography variant="h6">Valve 1</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
-                <Typography style={{marginTop: 80}} variant="h5">OFF</Typography>
-                <Grid item xs={12} md={6} style={{margin: 'auto'}}>
-                    <ButtonGroup variant="contained" color="secondary" fullWidth aria-label="full width outlined button group">
-                        <Button>Valve 1</Button>
-                        <Button>Valve 2</Button>
-                        <Button>Valve 3</Button>
-                        <Button>Valve 4</Button>
-                        <Button>Valve 5</Button>
-                        <Button>Valve 6</Button>
-                        <Button>Valve 7</Button>
-                        <Button>Valve 8</Button>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 2</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 3</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 4</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 5</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 6</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 7</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
+                    </ButtonGroup>
+                </Grid>
+                <Typography style={{marginTop: 20}} variant="h6">Valve 8</Typography>
+                <Grid item xs={1} style={{margin: 'auto'}}>
+                    <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
+                        <Button color="primary">ON</Button>
+                        <Button color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
             </div>
