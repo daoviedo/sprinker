fc2166b73b900482abf1989f381f0e8c16f27721
Joel Jijo <joeljijo123@gmail.com>
2019-11-21 11:39:40 -0600

    added api usage

--- frontend/src/Containers/Manual.js
+++ frontend/src/Containers/Manual.js
@@ -4,65 +4,84 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { Typography } from '@material-ui/core';
+import Valves from '../Components/Valves';
 
 class Manual extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    turnOn(valve){
+        fetch('http://192.168.1.9:8080/on/' + valve,{
+            method: "POST"
+        })
+        .then(res => console.log(res))
+    }
+    turnOff(valve){
+        fetch('http://192.168.1.9:8080/off/' + valve,{
+            method: "POST"
+        })
+        .then(res => console.log(res))
+    }
     render() {
         return (
             <div style={{marginTop: 30, textAlign: 'center'}}>
                 <Typography variant="h6">Valve 1</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(1)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(1)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 2</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(2)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(2)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 3</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(3)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(3)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 4</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(4)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(4)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 5</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(5)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(5)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 6</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(6)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(6)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 7</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(7)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(7)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
                 <Typography style={{marginTop: 20}} variant="h6">Valve 8</Typography>
                 <Grid item xs={1} style={{margin: 'auto'}}>
                     <ButtonGroup variant="contained" fullWidth aria-label="full width outlined button group">
-                        <Button color="primary">ON</Button>
-                        <Button color="secondary">OFF</Button>
+                        <Button onClick={() => this.turnOn(8)} color="primary">ON</Button>
+                        <Button onClick={() => this.turnOff(8)} color="secondary">OFF</Button>
                     </ButtonGroup>
                 </Grid>
             </div>
--- frontend/src/Containers/Schedule.js
+++ frontend/src/Containers/Schedule.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ScheduleStepper from '../Components/ScheduleStepper';
+import { Button } from '@material-ui/core';
 
 class Schedule extends Component {
     constructor(props){
@@ -18,6 +19,46 @@ class Schedule extends Component {
             numValves: ''
         }
     }
+    setSchedule(){
+        fetch('http://192.168.1.9:8080/schedule', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                startTime: this.state.startTime.toTimeString().split(" ")[0].substring(0,5),
+                duration: parseInt(this.state.duration),
+                days: this.dayArray(),
+                stations: parseInt(this.state.numValves)
+            })
+        })
+        .then(result => console.log(result))
+    }
+    dayArray(){
+        let tempArr = []
+        if(this.state.mon === true){
+            tempArr.push('Mon')
+        }
+        if(this.state.tue === true){
+            tempArr.push('Tue')
+        }
+        if(this.state.wed === true){
+            tempArr.push('Wed')
+        }
+        if(this.state.thu === true){
+            tempArr.push('Thu')
+        }
+        if(this.state.fri === true){
+            tempArr.push('Fri')
+        }
+        if(this.state.sat === true){
+            tempArr.push('Sat')
+        }
+        if(this.state.sun === true){
+            tempArr.push('Sun')
+        }
+        return tempArr;
+    }
     handleChange = name => event => {
         this.setState({[name]: event.target.checked });
     }
@@ -28,9 +69,13 @@ class Schedule extends Component {
         this.setState({[name]: event.target.value});
     }
     render() {
+        console.log(this.state.scheduleDays)
+        const newDay = new Date();
+        console.log(newDay.toTimeString().split(" ")[0].substring(0,5))
         return (
             <div>
                 <ScheduleStepper handleNumChange={this.handleNumChange} handleDateChange={this.handleDateChange} handleChange={this.handleChange} state={this.state}/>
+                <Button onClick={() => this.setSchedule()}>Test</Button>
             </div>
         );
     }
