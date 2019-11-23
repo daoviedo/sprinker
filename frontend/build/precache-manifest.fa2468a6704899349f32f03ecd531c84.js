dff9712a32db4e89e1d473b804758e2654b7aa79
daoviedo <44733952+daoviedo@users.noreply.github.com>
2019-11-23 16:33:50 -0600

    built

---
+++ frontend/build/precache-manifest.fa2468a6704899349f32f03ecd531c84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a3151e839adae92bea92339d073e3d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3e40a10947b8d83b368",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "96f2138876f5067b92fe",
+    "url": "/static/js/2.4c4e5643.chunk.js"
+  },
+  {
+    "revision": "f3e40a10947b8d83b368",
+    "url": "/static/js/main.29d003dd.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
